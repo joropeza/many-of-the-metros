61306765078e64ef008de1dbc08f1694003fbd8d
Jonny <joropeza@gmail.com>
2018-07-08 16:39:10 -0700

    greatly simplify structure

--- metroAreas.js
+++ metroAreas.js
@@ -1,252 +1,250 @@
 module.exports = {
-    "Metro Areas": {
-      "San_Francisco_Bay_Area": {
-        "ID": "San_Francisco_Bay_Area",
-        "Title": "San Francisco Bay Area",
-        "Time Zone": "Pacific_Time_Zone",
-        "Population": 7.76,
-        "Latitude": 37.45,
-        "Longitude": -122.17
-      },
-      "New_York_metropolitan_area": {
-        "ID": "New_York_metropolitan_area",
-        "Title": "New York Metro",
-        "Time Zone": "Eastern_Time_Zone",
-        "Population": 23.7,
-        "Latitude": 40.808611,
-        "Longitude": -74.020386
-      },
-      "Greater_Boston": {
-        "ID": "Greater_Boston",
-        "Title": "Greater Boston",
-        "Time Zone": "Eastern_Time_Zone",
-        "Population": 8.1,
-        "Latitude": 42.35817,
-        "Longitude": -71.06369
-      },
-      "Greater_Los_Angeles": {
-        "ID": "Greater_Los_Angeles",
-        "Title": "Greater Los Angeles",
-        "Time Zone": "Pacific_Time_Zone",
-        "Population": 18.7,
-        "Latitude": 35.0569,
-        "Longitude": -118.25
-      },
-      "San_Diego_County,_California": {
-        "ID": "San_Diego_County,_California",
-        "Title": "San Diego County",
-        "Time Zone": "Pacific_Time_Zone",
-        "Population": 3.1,
-        "Latitude": 33.02,
-        "Longitude": -116.77
-      },
-      "Seattle_metropolitan_area": {
-        "ID": "Seattle_metropolitan_area",
-        "Title": "Seattle Metro",
-        "Time Zone": "Pacific_Time_Zone",
-        "Population": 3.9,
-        "Latitude": 47.44,
-        "Longitude": -122.19
-      },
-      "Chicago_metropolitan_area": {
-        "ID": "Chicago_metropolitan_area",
-        "Title": "Chicago metropolitan area",
-        "Time Zone": "Central_Time_Zone",
-        "Population": 9.5,
-        "Latitude": 41.54,
-        "Longitude": -87.39
-      },
-      "Washington_metropolitan_area": {
-        "ID": "Washington_metropolitan_area",
-        "Title": "D.C. Area",
-        "Time Zone": "Eastern_Time_Zone",
-        "Population": 6.1,
-        "Latitude": 38.886758,
-        "Longitude": -77.041625
-      },
-      "Greater_Austin": {
-        "ID": "Greater_Austin",
-        "Title": "Greater Austin",
-        "Time Zone": "Central_Time_Zone",
-        "Population": 2.1,
-        "Latitude": 30.266667,
-        "Longitude": -97.733333
-      },
-      "Delaware_Valley": {
-        "ID": "Delaware_Valley",
-        "Title": "Delaware Valley",
-        "Time Zone": "Eastern_Time_Zone",
-        "Population": 7.1,
-        "Latitude": 39.877,
-        "Longitude": -75.323
-      },
-      "Atlanta_metropolitan_area": {
-        "ID": "Atlanta_metropolitan_area",
-        "Title": "Atlanta Metro",
-        "Time Zone": "Eastern_Time_Zone",
-        "Population": 6.5,
-        "Latitude": 33.8,
-        "Longitude": -84.4
-      },
-      "Miami_metropolitan_area": {
-        "ID": "Miami_metropolitan_area",
-        "Title": "Miami Metro",
-        "Time Zone": "Eastern_Time_Zone",
-        "Population": 6.2,
-        "Latitude": 26.133333,
-        "Longitude": -80.2
-      },
-      "Dallas–Fort_Worth_metroplex": {
-        "ID": "Dallas–Fort_Worth_metroplex",
-        "Title": "Dallas-Fort Worth",
-        "Time Zone": "Central_Time_Zone",
-        "Population": 6.8,
-        "Latitude": 32.783333,
-        "Longitude": -96.8
-      },
-      "Denver_metropolitan_area": {
-        "ID": "Denver_metropolitan_area",
-        "Title": "Denver Metro",
-        "Time Zone": "Mountain_Time_Zone",
-        "Population": 3.3,
-        "Latitude": 39.76185,
-        "Longitude": -104.881105
-      },
-      "Minneapolis_metropolitan_area": {
-        "ID": "Minneapolis_metropolitan_area",
-        "Title": "Minneapolis–Saint Paul",
-        "Time Zone": "Central_Time_Zone",
-        "Population": 3.9,
-        "Latitude": 44.95,
-        "Longitude": -93.2
-      },
-      "Greater_Houston": {
-        "ID": "Greater_Houston",
-        "Title": "Greater Houston",
-        "Time Zone": "Central_Time_Zone",
-        "Population": 6.5,
-        "Latitude": 29.762778,
-        "Longitude": -95.383056
-      },
-      "Portland_metropolitan_area": {
-        "ID": "Portland_metropolitan_area",
-        "Title": "Portland Metro",
-        "Time Zone": "Pacific_Time_Zone",
-        "Population": 2.5,
-        "Latitude": 45.5,
-        "Longitude": -122.65
-      },
-      "Research_Triangle": {
-        "ID": "Research_Triangle",
-        "Title": "Raleigh-Durham-Chapel Hill",
-        "Time Zone": "Eastern_Time_Zone",
-        "Population": 2.2,
-        "Latitude": 35.766667,
-        "Longitude": -78.633333
-      },
-      "Metro_Detroit": {
-        "ID": "Metro_Detroit",
-        "Title": "Metro Detroit",
-        "Time Zone": "Eastern_Time_Zone",
-        "Population": 5.3,
-        "Latitude": 42.331389,
-        "Longitude": -83.045833
-      },
-      "Phoenix_metropolitan_area": {
-        "ID": "Phoenix_metropolitan_area",
-        "Title": "Phoenix Metro",
-        "Time Zone": "Mountain_Time_Zone",
-        "Population": 4.7,
-        "Latitude": 33.17,
-        "Longitude": -112.04
-      },
-      "Tampa_Bay_Area": {
-        "ID": "Tampa_Bay_Area",
-        "Title": "Tampa Bay Area",
-        "Time Zone": "Eastern_Time_Zone",
-        "Population": 3.1,
-        "Latitude": 28,
-        "Longitude": -82.3
-      },
-      "Greater_St._Louis": {
-        "ID": "Greater_St._Louis",
-        "Title": "Greater St. Louis",
-        "Time Zone": "Central_Time_Zone",
-        "Population": 2.9,
-        "Latitude": 38.627222,
-        "Longitude": -90.197778
-      },
-      "Greater_Pittsburgh_Region": {
-        "ID": "Greater_Pittsburgh_Region",
-        "Title": "Greater Pittsburgh",
-        "Time Zone": "Eastern_Time_Zone",
-        "Population": 2.6,
-        "Latitude": 40.421,
-        "Longitude": -79.984
-      },
-      "Cincinnati_metropolitan_area": {
-        "ID": "Cincinnati_metropolitan_area",
-        "Title": "Greater Cincinnati",
-        "Time Zone": "Eastern_Time_Zone",
-        "Population": 2.2,
-        "Latitude": 39.1,
-        "Longitude": -84.516667
-      },
-      "Sacramento_metropolitan_area": {
-        "ID": "Sacramento_metropolitan_area",
-        "Title": "Greater Sacramento",
-        "Time Zone": "Pacific_Time_Zone",
-        "Population": 2.5,
-        "Latitude": 38.555556,
-        "Longitude": -121.468889
-      },
-      "Greater_Cleveland": {
-        "ID": "Greater_Cleveland",
-        "Title": "Greater Cleveland",
-        "Time Zone": "Eastern_Time_Zone",
-        "Population": 3.5,
-        "Latitude": 41.482222,
-        "Longitude": -81.669722
-      },
-      "Greater_Orlando": {
-        "ID": "Greater_Orlando",
-        "Title": "Greater Orlando",
-        "Time Zone": "Eastern_Time_Zone",
-        "Population": 2.4,
-        "Latitude": 28.54,
-        "Longitude": -81.38
-      },
-      "Greater_San_Antonio": {
-        "ID": "Greater_San_Antonio",
-        "Title": "Greater San Antonio",
-        "Time Zone": "Central_Time_Zone",
-        "Population": 2.4,
-        "Latitude": 29.416667,
-        "Longitude": -98.5
-      },
-      "Kansas_City_metropolitan_area": {
-        "ID": "Kansas_City_metropolitan_area",
-        "Title": "Kansas City Metropolitan Area",
-        "Time Zone": "Central_Time_Zone",
-        "Population": 2.5,
-        "Latitude": 39.1,
-        "Longitude": -94.58
-      },
-      "Las_Vegas_Valley": {
-        "ID": "Las_Vegas_Valley",
-        "Title": "Las Vegas Valley",
-        "Time Zone": "Pacific_Time_Zone",
-        "Population": 2,
-        "Latitude": 36.1318,
-        "Longitude": -115.184
-      },
-      "Nashville_metropolitan_area": {
-        "ID": "Nashville_metropolitan_area",
-        "Title": "Nashville Metropolitan Area",
-        "Time Zone": "Central_Time_Zone",
-        "Population": 1.9,
-        "Latitude": 36.1,
-        "Longitude": -86.9
-      }
-    }
+  "San_Francisco_Bay_Area": {
+    "ID": "San_Francisco_Bay_Area",
+    "Title": "San Francisco Bay Area",
+    "Time Zone": "Pacific_Time_Zone",
+    "Population": 7.76,
+    "Latitude": 37.45,
+    "Longitude": -122.17
+  },
+  "New_York_metropolitan_area": {
+    "ID": "New_York_metropolitan_area",
+    "Title": "New York Metro",
+    "Time Zone": "Eastern_Time_Zone",
+    "Population": 23.7,
+    "Latitude": 40.808611,
+    "Longitude": -74.020386
+  },
+  "Greater_Boston": {
+    "ID": "Greater_Boston",
+    "Title": "Greater Boston",
+    "Time Zone": "Eastern_Time_Zone",
+    "Population": 8.1,
+    "Latitude": 42.35817,
+    "Longitude": -71.06369
+  },
+  "Greater_Los_Angeles": {
+    "ID": "Greater_Los_Angeles",
+    "Title": "Greater Los Angeles",
+    "Time Zone": "Pacific_Time_Zone",
+    "Population": 18.7,
+    "Latitude": 35.0569,
+    "Longitude": -118.25
+  },
+  "San_Diego_County,_California": {
+    "ID": "San_Diego_County,_California",
+    "Title": "San Diego County",
+    "Time Zone": "Pacific_Time_Zone",
+    "Population": 3.1,
+    "Latitude": 33.02,
+    "Longitude": -116.77
+  },
+  "Seattle_metropolitan_area": {
+    "ID": "Seattle_metropolitan_area",
+    "Title": "Seattle Metro",
+    "Time Zone": "Pacific_Time_Zone",
+    "Population": 3.9,
+    "Latitude": 47.44,
+    "Longitude": -122.19
+  },
+  "Chicago_metropolitan_area": {
+    "ID": "Chicago_metropolitan_area",
+    "Title": "Chicago metropolitan area",
+    "Time Zone": "Central_Time_Zone",
+    "Population": 9.5,
+    "Latitude": 41.54,
+    "Longitude": -87.39
+  },
+  "Washington_metropolitan_area": {
+    "ID": "Washington_metropolitan_area",
+    "Title": "D.C. Area",
+    "Time Zone": "Eastern_Time_Zone",
+    "Population": 6.1,
+    "Latitude": 38.886758,
+    "Longitude": -77.041625
+  },
+  "Greater_Austin": {
+    "ID": "Greater_Austin",
+    "Title": "Greater Austin",
+    "Time Zone": "Central_Time_Zone",
+    "Population": 2.1,
+    "Latitude": 30.266667,
+    "Longitude": -97.733333
+  },
+  "Delaware_Valley": {
+    "ID": "Delaware_Valley",
+    "Title": "Delaware Valley",
+    "Time Zone": "Eastern_Time_Zone",
+    "Population": 7.1,
+    "Latitude": 39.877,
+    "Longitude": -75.323
+  },
+  "Atlanta_metropolitan_area": {
+    "ID": "Atlanta_metropolitan_area",
+    "Title": "Atlanta Metro",
+    "Time Zone": "Eastern_Time_Zone",
+    "Population": 6.5,
+    "Latitude": 33.8,
+    "Longitude": -84.4
+  },
+  "Miami_metropolitan_area": {
+    "ID": "Miami_metropolitan_area",
+    "Title": "Miami Metro",
+    "Time Zone": "Eastern_Time_Zone",
+    "Population": 6.2,
+    "Latitude": 26.133333,
+    "Longitude": -80.2
+  },
+  "Dallas–Fort_Worth_metroplex": {
+    "ID": "Dallas–Fort_Worth_metroplex",
+    "Title": "Dallas-Fort Worth",
+    "Time Zone": "Central_Time_Zone",
+    "Population": 6.8,
+    "Latitude": 32.783333,
+    "Longitude": -96.8
+  },
+  "Denver_metropolitan_area": {
+    "ID": "Denver_metropolitan_area",
+    "Title": "Denver Metro",
+    "Time Zone": "Mountain_Time_Zone",
+    "Population": 3.3,
+    "Latitude": 39.76185,
+    "Longitude": -104.881105
+  },
+  "Minneapolis_metropolitan_area": {
+    "ID": "Minneapolis_metropolitan_area",
+    "Title": "Minneapolis–Saint Paul",
+    "Time Zone": "Central_Time_Zone",
+    "Population": 3.9,
+    "Latitude": 44.95,
+    "Longitude": -93.2
+  },
+  "Greater_Houston": {
+    "ID": "Greater_Houston",
+    "Title": "Greater Houston",
+    "Time Zone": "Central_Time_Zone",
+    "Population": 6.5,
+    "Latitude": 29.762778,
+    "Longitude": -95.383056
+  },
+  "Portland_metropolitan_area": {
+    "ID": "Portland_metropolitan_area",
+    "Title": "Portland Metro",
+    "Time Zone": "Pacific_Time_Zone",
+    "Population": 2.5,
+    "Latitude": 45.5,
+    "Longitude": -122.65
+  },
+  "Research_Triangle": {
+    "ID": "Research_Triangle",
+    "Title": "Raleigh-Durham-Chapel Hill",
+    "Time Zone": "Eastern_Time_Zone",
+    "Population": 2.2,
+    "Latitude": 35.766667,
+    "Longitude": -78.633333
+  },
+  "Metro_Detroit": {
+    "ID": "Metro_Detroit",
+    "Title": "Metro Detroit",
+    "Time Zone": "Eastern_Time_Zone",
+    "Population": 5.3,
+    "Latitude": 42.331389,
+    "Longitude": -83.045833
+  },
+  "Phoenix_metropolitan_area": {
+    "ID": "Phoenix_metropolitan_area",
+    "Title": "Phoenix Metro",
+    "Time Zone": "Mountain_Time_Zone",
+    "Population": 4.7,
+    "Latitude": 33.17,
+    "Longitude": -112.04
+  },
+  "Tampa_Bay_Area": {
+    "ID": "Tampa_Bay_Area",
+    "Title": "Tampa Bay Area",
+    "Time Zone": "Eastern_Time_Zone",
+    "Population": 3.1,
+    "Latitude": 28,
+    "Longitude": -82.3
+  },
+  "Greater_St._Louis": {
+    "ID": "Greater_St._Louis",
+    "Title": "Greater St. Louis",
+    "Time Zone": "Central_Time_Zone",
+    "Population": 2.9,
+    "Latitude": 38.627222,
+    "Longitude": -90.197778
+  },
+  "Greater_Pittsburgh_Region": {
+    "ID": "Greater_Pittsburgh_Region",
+    "Title": "Greater Pittsburgh",
+    "Time Zone": "Eastern_Time_Zone",
+    "Population": 2.6,
+    "Latitude": 40.421,
+    "Longitude": -79.984
+  },
+  "Cincinnati_metropolitan_area": {
+    "ID": "Cincinnati_metropolitan_area",
+    "Title": "Greater Cincinnati",
+    "Time Zone": "Eastern_Time_Zone",
+    "Population": 2.2,
+    "Latitude": 39.1,
+    "Longitude": -84.516667
+  },
+  "Sacramento_metropolitan_area": {
+    "ID": "Sacramento_metropolitan_area",
+    "Title": "Greater Sacramento",
+    "Time Zone": "Pacific_Time_Zone",
+    "Population": 2.5,
+    "Latitude": 38.555556,
+    "Longitude": -121.468889
+  },
+  "Greater_Cleveland": {
+    "ID": "Greater_Cleveland",
+    "Title": "Greater Cleveland",
+    "Time Zone": "Eastern_Time_Zone",
+    "Population": 3.5,
+    "Latitude": 41.482222,
+    "Longitude": -81.669722
+  },
+  "Greater_Orlando": {
+    "ID": "Greater_Orlando",
+    "Title": "Greater Orlando",
+    "Time Zone": "Eastern_Time_Zone",
+    "Population": 2.4,
+    "Latitude": 28.54,
+    "Longitude": -81.38
+  },
+  "Greater_San_Antonio": {
+    "ID": "Greater_San_Antonio",
+    "Title": "Greater San Antonio",
+    "Time Zone": "Central_Time_Zone",
+    "Population": 2.4,
+    "Latitude": 29.416667,
+    "Longitude": -98.5
+  },
+  "Kansas_City_metropolitan_area": {
+    "ID": "Kansas_City_metropolitan_area",
+    "Title": "Kansas City Metropolitan Area",
+    "Time Zone": "Central_Time_Zone",
+    "Population": 2.5,
+    "Latitude": 39.1,
+    "Longitude": -94.58
+  },
+  "Las_Vegas_Valley": {
+    "ID": "Las_Vegas_Valley",
+    "Title": "Las Vegas Valley",
+    "Time Zone": "Pacific_Time_Zone",
+    "Population": 2,
+    "Latitude": 36.1318,
+    "Longitude": -115.184
+  },
+  "Nashville_metropolitan_area": {
+    "ID": "Nashville_metropolitan_area",
+    "Title": "Nashville Metropolitan Area",
+    "Time Zone": "Central_Time_Zone",
+    "Population": 1.9,
+    "Latitude": 36.1,
+    "Longitude": -86.9
   }
+}
